1fed434c00083f053678f487e7d7f6e4e3f4d26a
Lesnar Dsilva <lesnar.dsilva@gmail.com>
2021-07-25 09:24:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let button = document.getElementById("button");
+
+function changeColor (){
+	let rgb = Math.floor(Math.random() * 16777216).toString(16); //toString(16) returns a hex value if the resulting value is between 10 and 15
+	document.body.style.backgroundColor = `#${rgb}`;
+	button.textContent = `Background Color: #${rgb}`;
+}
+
+button.style.border = "none";
+button.style.fontSize = "100px";
+
+button.addEventListener("click", changeColor);
