87da93c50ae0a00f456a786da3478ede79060e58
Lesnar Dsilva <lesnar.dsilva@gmail.com>
2021-09-08 09:36:24 +0100

    Add files via upload

---
+++ Grocery List/index.js
@@ -0,0 +1,30 @@
+let form = document.getElementById("form");
+let list = document.getElementById("list");
+let input = document.getElementById("newItem");
+let submit = document.getElementById("submit");
+
+let item = document.getElementsByTagName("li");
+let button = document.getElementsByTagName("button");
+
+let id = 4;
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (input.value.length >= 1) {
+    input.style.borderColor = "green";
+    id++;
+    list.innerHTML += "<li id=" + "No" + id + ">" + input.value + "</li>";
+  } else {
+    input.style.borderColor = "red";
+  }
+});
+
+for (let i = 0; i < item.length; i++) {
+  item[i].addEventListener("click", (e) => {
+    e.target.style.textDecoration = "line-through";
+  });
+  item[i].addEventListener(
+    "dblclick",
+    (e) => (e.target.style.textDecoration = "none")
+  );
+}
